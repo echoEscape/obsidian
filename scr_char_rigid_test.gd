24d33280dbca9a9b94674b114c0c2688b97ced67
echoEscape <thenumberiszero@gmail.com>
2024-01-12 21:58:39 +0100

    Implement camera mouse movement

--- scr_char_rigid_test.gd
+++ scr_char_rigid_test.gd
@@ -1,5 +1,8 @@
 extends RigidBody3D
 
+var mouse_sensitivity := 0.001
+var twist_input := 0.0
+var pitch_input := 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,3 +19,21 @@ func _process(delta):
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	$TwistPivot.rotate_y(twist_input )
+	$TwistPivot/PitchPivot.rotate_x(pitch_input)
+	#Limit rotation amount
+	$TwistPivot/PitchPivot.rotation.x = clamp(
+		$TwistPivot/PitchPivot.rotation.x,
+		-0.5,
+		0.5
+	)
+	#Stop camera movement when you stop the mouse movement
+	twist_input = 0.0
+	pitch_input = 0.0
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			twist_input = - event.relative.x * mouse_sensitivity
+			pitch_input = - event.relative.y * mouse_sensitivity

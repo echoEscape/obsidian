46ade088d39eeeb00b836220df59b96411d377f8
echoEscape <thenumberiszero@gmail.com>
2024-01-12 22:07:54 +0100

    Turn character with camera for WASD by using basis

--- scr_char_rigid_test.gd
+++ scr_char_rigid_test.gd
@@ -4,6 +4,9 @@ var mouse_sensitivity := 0.001
 var twist_input := 0.0
 var pitch_input := 0.0
 
+@onready var onr_twist_pivot := $TwistPivot
+@onready var onr_pitch_pivot := $TwistPivot/PitchPivot
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -15,19 +18,18 @@ func _process(delta):
 	input.x = Input.get_axis("move_left", "move_right")
 	input.z = Input.get_axis("move_forward", "move_backward")
 
-	apply_central_force(input * 1200.0 * delta)
+	#Use Basis of Camera to move character with camera for WASD adjusting to where camera is looking
+	apply_central_force(onr_twist_pivot.basis * input * 1200.0 * delta)
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
-	$TwistPivot.rotate_y(twist_input )
-	$TwistPivot/PitchPivot.rotate_x(pitch_input)
+	onr_twist_pivot.rotate_y(twist_input )
+	onr_pitch_pivot.rotate_x(pitch_input)
 	#Limit rotation amount
-	$TwistPivot/PitchPivot.rotation.x = clamp(
-		$TwistPivot/PitchPivot.rotation.x,
-		-0.5,
-		0.5
-	)
+	onr_pitch_pivot.rotation.x = clamp(onr_pitch_pivot.rotation.x, 
+	deg_to_rad(-30),
+	deg_to_rad(30))
 	#Stop camera movement when you stop the mouse movement
 	twist_input = 0.0
 	pitch_input = 0.0

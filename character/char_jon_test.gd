cfe31f4b45150765abe72a9c85efae567087cd10
echoEscape <thenumberiszero@gmail.com>
2024-01-17 20:31:56 +0100

    Move over camera movement from test script -> Char doesnt move with camera

--- character/char_jon_test.gd
+++ character/char_jon_test.gd
@@ -1,14 +1,35 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+var mouse_sensitivity := 0.001
+var twist_input := 0.0
+var pitch_input := 0.0
+
+@onready var onr_twist_pivot := $TwistPivot
+@onready var onr_pitch_pivot := $TwistPivot/PitchPivot
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	onr_twist_pivot.rotate_y(twist_input )
+	onr_pitch_pivot.rotate_x(pitch_input)
+	#Limit rotation amount
+	onr_pitch_pivot.rotation.x = clamp(onr_pitch_pivot.rotation.x, 
+	deg_to_rad(-30),
+	deg_to_rad(30))
+	#Stop camera movement when you stop the mouse movement
+	twist_input = 0.0
+	pitch_input = 0.0
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -19,7 +40,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -29,3 +50,10 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			twist_input = - event.relative.x * mouse_sensitivity
+			pitch_input = - event.relative.y * mouse_sensitivity
+

d5cb9e76ddd68fde50aef1f978bc6a50c4e0f7e7
echoEscape <thenumberiszero@gmail.com>
2024-01-23 21:17:11 +0100

    Add running

---
+++ script/scr_char_jon_2.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export var speed = 400
+
+func walk(bool_shift:bool):
+	var input_direction = Input.get_vector("left_a", "right_d", "up_w", "down_s")
+	
+	if bool_shift:
+		speed = 1000
+	else:
+		speed = 750
+
+	velocity = input_direction * speed
+
+func get_input():
+	if Input.is_action_pressed("shift"):
+		walk(true)
+	else:
+		walk(false)
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()

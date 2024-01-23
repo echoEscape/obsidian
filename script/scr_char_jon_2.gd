a9105d7e72a68fd538b7c873a99176e3e8c73cdf
echoEscape <thenumberiszero@gmail.com>
2024-01-23 22:01:38 +0100

    Add basic logic for tile based movement

--- script/scr_char_jon_2.gd
+++ script/scr_char_jon_2.gd
@@ -1,23 +1,36 @@
 extends CharacterBody2D
 
-@export var speed = 400
+var movement : Vector2 = Vector2()
+@export var move_speed : int = 32
+@export var facing : String = "down"
+var talking : bool = false
 
-func walk(bool_shift:bool):
-	var input_direction = Input.get_vector("left_a", "right_d", "up_w", "down_s")
+func _physics_process(delta) -> void:
+	if !talking:
+		# Moving
+		if Input.is_action_pressed("right_d"):
+			facing = "right"
+			movement.x = move_speed
+			movement.y = 0
+		elif Input.is_action_pressed("left_a"):
+			facing = "left"
+			movement.x = -move_speed
+			movement.y = 0
+		elif Input.is_action_pressed("up_w"):
+			facing = "up"
+			movement.x = 0
+			movement.y = move_speed
+		elif Input.is_action_pressed("down_s"):
+			facing = "down"
+			movement.x = 0
+			movement.y = -move_speed
+		else:
+			# Idle
+			pass
+		
+		move_and_slide()
+	else:
+		pass
 	
-	if bool_shift:
-		speed = 750
-	else:
-		speed = 500
-
-	velocity = input_direction * speed
-
-func get_input():
-	if Input.is_action_pressed("shift"):
-		walk(true)
-	else:
-		walk(false)
-
-func _physics_process(delta):
-	get_input()
+	
 	move_and_slide()

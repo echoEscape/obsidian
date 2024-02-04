a1e4c63391c8d4403cc6ba3aee2f66c2affc381d
echoEscape <thenumberiszero@gmail.com>
2024-02-04 21:58:30 +0100

    remove old test files and create basic environment instead

--- script/global.gd
+++ script/global.gd
@@ -1,15 +1,11 @@
 extends Node
 
-var current_scene : String = "env_a001_jon_office"
-var transition_scene : bool = false
 
-var player_exit_pos : Vector2 = Vector2(0,0)
-var player_start_pos : Vector2 =  Vector2(0,0)
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
 
-func finish_scene_change() -> void:
-	if transition_scene:
-		transition_scene = false
-		if current_scene == "env_a001_jon_office":
-			current_scene = "env_a002_hallway"
-		else:
-			current_scene = "env_a001_jon_office" 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass

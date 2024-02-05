0b17e922ec226f5a8af44160f855bdb1c0e78ed7
echoEscape <thenumberiszero@gmail.com>
2024-02-05 20:36:40 +0100

    Add player character script

--- character/char_player.gd
+++ character/char_player.gd
@@ -24,8 +24,8 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("left_a", "right_d", "up_w", "down_s")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * SPEED * SPEED_UP
+		velocity.z = direction.z * SPEED * SPEED_UP
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
